15e38ab11eb18e42628b7e14fbca92385b110722
Javid Izadfar <javid.izadfar@gmail.com>
2016-11-25 18:15:26 +0330

    Added ProgressBar

--- vida.js
+++ vida.js
@@ -1,13 +1,21 @@
 function vida(element) {
    var video = $(element)[0];
+   var interval;
+   var duration = video.duration;
+
+   function  updateProgress() {
+      $('.vida-play-progressbar .vida-play-passed').css('width', (video.currentTime / duration) * 100 + '%')
+   }
 
    function playPause() {
        if (video.paused){
           video.play();
-          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause')
+          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause');
+          interval = setInterval(function(){ updateProgress() }, 500);
        } else {
           video.pause();
-          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause')
+          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause');
+          clearInterval(interval)
        }
    }
 
@@ -16,12 +24,10 @@ function vida(element) {
       playPause()
    })
 
-   $('.vida-video').on('click', function() {
-      playPause()
-   })
-
-   $('.vida-controls .vida-play-icon').on('click', function() {
+   $('.vida-video, .vida-controls .vida-play-icon').on('click', function() {
       playPause()
    })
 
 }
+
+vida('#video1');
