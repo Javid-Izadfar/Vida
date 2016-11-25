dcca5c5d0c7b7e881c0d23a15c2ebea1e481815a
Javid Izadfar <javid.izadfar@gmail.com>
2016-11-25 17:46:13 +0330

    Added Play and Pause button

--- vida.js
+++ vida.js
@@ -2,18 +2,25 @@ function vida(element) {
    var video = $(element)[0];
 
    function playPause() {
-       if (video.paused)
-           video.play();
-       else
-           video.pause();
+       if (video.paused){
+          video.play();
+          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause')
+       } else {
+          video.pause();
+          $('.vida-controls .vida-play-icon .gb').toggleClass('gb_play_arrow gb_pause')
+       }
    }
 
    $('.vida-poster').on('click', function() {
       $('.vida-poster').fadeOut()
-      video.play()
+      playPause()
    })
 
-   $('.vida-status').on('click', function() {
+   $('.vida-video').on('click', function() {
+      playPause()
+   })
+
+   $('.vida-controls .vida-play-icon').on('click', function() {
       playPause()
    })
 
