bd9cf1813fb788adb088e81ae269da9ae9580613
Javid Izadfar <javid.izadfar@gmail.com>
2016-11-25 19:40:49 +0330

    Added Timer

--- vida.js
+++ vida.js
@@ -2,9 +2,33 @@ function vida(element) {
    var video = $(element)[0];
    var interval;
    var duration = video.duration;
+   var has_hour = true;
+
+   function secToHMS(secs, type) {
+      var sec_num = parseInt(secs, 10);
+      var hours   = Math.floor(sec_num / 3600);
+      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+      if ( type == 'duration' && hours == 0) {
+         has_hour = false;
+      }
+
+      if ( has_hour ) {
+         if (hours < 10) {hours   = '0' + hours + ':';}
+      } else {
+         hours = '';
+      }
+      if (minutes < 10) {minutes = '0' + minutes + ':';}
+      if (seconds < 10) {seconds = '0' + seconds;}
+      return hours + minutes + seconds;
+   }
+
+   $('.vida-play-duration').text(secToHMS(duration, 'duration'))
 
    function  updateProgress() {
       $('.vida-play-progressbar .vida-play-passed').css('width', (video.currentTime / duration) * 100 + '%')
+      $('.vida-play-elapsed').text(secToHMS(video.currentTime, 'elaped'))
    }
 
    function playPause() {
